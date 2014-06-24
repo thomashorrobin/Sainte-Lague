70b727dcad0764d310ed1720c5f685648389dad6
Bunnykins <thomasroberthorrobin@gmail.com>
2014-06-24 22:50:18 +1200

    added 2008 election to tests

--- Sainte-Lague accuracy tests/UnitTest1.cs
+++ Sainte-Lague accuracy tests/UnitTest1.cs
@@ -30,5 +30,26 @@ namespace Sainte_Lague_accuracy_tests
             Assert.IsTrue(er.SeatsCalculated(7) == 1);
             Assert.IsTrue(er.SeatsCalculated(8) == 1);
         }
+
+        [TestMethod]
+        public void Election2008()
+        {
+            ElectionResult er = new ElectionResult();
+            er.AddParty(new Party(1053398, 1));
+            er.AddParty(new Party(796880, 2));
+            er.AddParty(new Party(157613, 3));
+            er.AddParty(new Party(55980, 4, 5));
+            er.AddParty(new Party(21241, 5));
+            er.AddParty(new Party(85496, 6));
+            er.AddParty(new Party(20497, 7));
+            er.CalculateSeats();
+            Assert.IsTrue(er.SeatsCalculated(1) == 58, "Hasn't worked for National");
+            Assert.IsTrue(er.SeatsCalculated(2) == 43, "Hasn't worked for Labour");
+            Assert.IsTrue(er.SeatsCalculated(3) == 9);
+            Assert.IsTrue(er.SeatsCalculated(4) == 5, "Hasn't worked for the Maori Party");
+            Assert.IsTrue(er.SeatsCalculated(5) == 1);
+            Assert.IsTrue(er.SeatsCalculated(6) == 5);
+            Assert.IsTrue(er.SeatsCalculated(7) == 1);
+        }
     }
 }
